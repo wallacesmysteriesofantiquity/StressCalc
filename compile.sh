272ce24a6dd654df5d49161dbada2aa50ae31d92
wallacesmysteriesofantiquity <wallacesmysteriesofantiquity@gmail.com>
2024-12-30 12:18:18 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-x86_64-w64-mingw32-g++.exe -mwindows ./stresscalc.cpp ./Graphics/lodepng.cpp  -Wall -Wextra -pedantic -static -O3 -o ../stresscalc.exe
+x86_64-w64-mingw32-g++.exe -mwindows ./stresscalc.cpp ./Graphics/lodepng.cpp  -Wall -Wextra -pedantic -static -O3 -o ./stresscalc.exe
